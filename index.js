d732cb6251cbf2aec5b5651c416cd21f6463bb50
Kawser Rahman <shoriful521399@gmail.com>
2022-10-14 22:36:33 +0600

    erroro solve

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ app.use('/api', serviceRoutes)
 app.use('/api', storyRoutes)
 
 
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 app.get("/api", (rq, res)=>{
     res.send("TIC Limited Servcer")
 })
